8cc2c1f378ffb55e783f63a1c2c3d63243d96d77
Daniel Garrett <djgarret@go.olemiss.edu>
2015-01-07 20:59:45 -0600

    add fib_generator method

--- py2/EulerRunner.py
+++ py2/EulerRunner.py
@@ -6,3 +6,12 @@ def solve_problem(solve_func):
     end = datetime.datetime.now()
     print 'The answer is "' + result + '". Solved in ' + str((end - start).total_seconds()) + 's'
 
+def fib_generator():
+    fib1, fib2 = 0, 1
+    yield fib1
+    yield fib2
+    while True:
+        fibtemp = fib1 + fib2
+        fib1 = fib2
+        fib2 = fibtemp
+        yield fib2
